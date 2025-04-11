87c5b7aa38e60eeea96147c6bea52acb9f4955cc
Rimpei Maeta <maeta.rinpei@it-sp.co.jp>
2025-04-12 01:22:55 +0900

    コピーライト ヘッダー調整

--- page/layout/footer.php
+++ page/layout/footer.php
@@ -1,5 +1,5 @@
 <div class=<?php echo $CLASS_NAME_FOOTER; ?>>
     <footer>
-        <p class="footer__copyright">Copyright 2024 Primrosegarden All Rights Reserved.</p>
+        <p class="footer__copyright">Copyright <?php echo date('Y'); ?> Primrosegarden All Rights Reserved.</p>
     </footer>
 </div>
